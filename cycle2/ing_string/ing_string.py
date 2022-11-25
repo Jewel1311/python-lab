3579e017f41d64503862a77d4da6b55f1e60d43c
Jewel <84492029+Jewel1311@users.noreply.github.com>
2022-11-25 10:33:53 +0530

    Add files via upload

---
+++ cycle2/ing_string/ing_string.py
@@ -0,0 +1,13 @@
+def ing(l):
+    nl = []
+    for w in l:
+        if w[-3:] == "ing":
+            nl.append(w + "ly")
+        else:
+            nl.append(w + "ing")
+        
+
+    print(nl)
+
+l = input("Enter the string : ").split()
+ing(l)
