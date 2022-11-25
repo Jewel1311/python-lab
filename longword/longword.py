b1d526b93642188e91a04a583ce6ac37da386ab7
Jewel <84492029+Jewel1311@users.noreply.github.com>
2022-11-25 09:52:46 +0530

    Delete longword.py

--- longword/longword.py
+++
@@ -1,9 +0,0 @@
-def longword(l):
-    longest = l[0]
-    for w in l:
-        if len(w) > len(longest):
-            longest = w
-    print("Longest word is : ", longest)
-
-l = input("Enter the string : ").split()
-longword(l)
